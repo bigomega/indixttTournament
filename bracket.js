8e46cbe657acdc210fb9dad4530c8526d428cf04
Bharath <bharathraja@live.com>
2013-03-07 23:50:09 +0530

    initial commit

---
+++ bracket.js
@@ -0,0 +1,257 @@
+bracket = {
+  "name": "Winner",
+  "winners": [{
+      "name": "Finalists",
+      "winners": [{
+          "name": "SemiFinalists",
+          "winners": [{
+              "name": "quarterFinalists",
+              "winners": [{
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "round-2",
+                      "date": "xx-03-2013",
+                      "winners": [{
+                          "name": "Arun_Buduri",
+                        },
+                        {
+                          "name": "Zahoor"
+                        }]
+                    },
+                    {
+                      "name": "round-2",
+                      "winners": [{
+                          "name": "Vinoth",
+                        },
+                        {
+                          "name": "Chaitanya"
+                        }]
+                    }]
+                },
+                {
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "round-2",
+                      "winners": [{
+                          "name": "Prem",
+                        },
+                        {
+                          "name": "Arun"
+                        }]
+                    },
+                    {
+                      "name": "Praveen"
+                    }]
+                }]
+            },
+            {
+              "name": "quarterFinalists",
+              "winners": [{
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "Kishore"
+                    },
+                    {
+                      "name": "Pratik"
+                    }]
+                },
+                {
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "Thiru"
+                    },
+                    {
+                      "name": "Roshan"
+                    }]
+                }]
+            }]
+        },
+
+        {
+          "name": "SemiFinalists",
+          "winners": [{
+              "name": "quarterFinalists",
+              "winners": [{
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "Abishek"
+                    },
+                    {
+                      "name": "Karthika"
+                    }]
+                },
+                {
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "Vasanth"
+                    },
+                    {
+                      "name": "Sridhar"
+                    }]
+                }]
+            },
+            {
+              "name": "quarterFinalists",
+              "winners": [{
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "Bharath"
+                    },
+                    {
+                      "name": "Arul"
+                    }]
+                },
+                {
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "Rajesh"
+                    },
+                    {
+                      "name": "Srikanth"
+                    }]
+                }]
+            }]
+        }]
+    },
+
+    {
+      "name": "Finalists",
+      "winners": [{
+          "name": "SemiFinalists",
+          "winners": [{
+              "name": "quarterFinalists",
+              "winners": [{
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "Ashwanth"
+                    },
+                    {
+                      "name": "Bhavna"
+                    }]
+                },
+                {
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "Sanjay"
+                    },
+                    {
+                      "name": "Venkatesh"
+                    }]
+                }]
+            },
+            {
+              "name": "quarterFinalists",
+              "winners": [{
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "Jeevan"
+                    },
+                    {
+                      "name": "Aruna"
+                    }]
+                },
+                {
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "Anirudh"
+                    },
+                    {
+                      "name": "Mayank"
+                    }]
+                }]
+            }]
+        },
+        {
+          "name": "SemiFinalists",
+          "winners": [{
+              "name": "quarterFinalists",
+              "winners": [{
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "Sriram"
+                    },
+                    {
+                      "name": "Satya"
+                    }]
+                },
+                {
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "Jonah"
+                    },
+                    {
+                      "name": "Manoj"
+                    }]
+                }]
+            },
+            {
+              "name": "quarterFinalists",
+              "winners": [{
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "Rajesh_Mupalla"
+                    },
+                    {
+                      "name": "Sandipan"
+                    }]
+                },
+                {
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "Archanaa"
+                    },
+                    {
+                      "name": "Pranesh"
+                    }]
+                }]
+            }]
+        }]
+    }]
+}
+
+  imagesList = {
+    "Winner": "winner_profile.png",
+    "Finalists": "empty_profile.gif",
+    "SemiFinalists": "empty_profile.gif",
+    "quarterFinalists": "empty_profile.gif",
+    "round-1": "empty_profile.gif",
+    "round-2": "empty_profile.gif",
+    "round-3": "empty_profile.gif",
+    "round-4": "empty_profile.gif",
+    "Sanjay": "employee_list/0001 - SanjayParthasarathy.jpg",
+    Satya: "employee_list/0004 SatyaKaliki.jpg",
+    Jonah: "employee_list/0003 Jonah.jpg",
+    Rajesh_Mupalla: "employee_list/0005 RajeshMupalla.jpg",
+    Srikanth: "employee_list/0006 Srikanth.jpg",
+    Praveen: "employee_list/0008 Praveen.jpg",
+    Anirudh: "employee_list/0009 Anirudh.jpg",
+    Rajesh: "employee_list/0010 RajeshManikka.jpg",
+    Sriram: "employee_list/0011 Sriram.jpg",
+    Prem: "employee_list/0012 prem.jpg",
+    Kishore: "employee_list/0013 kishore.jpg",
+    Karthika: "employee_list/0014 Karthika_Seles.jpg",
+    Archanaa: "employee_list/0015 Archanaa.jpg",
+    Aruna: "employee_list/0016 Aruna.jpg",
+    Sandipan: "employee_list/0017 Sandipan.jpg",
+    Ashwanth: "employee_list/0019 Ashwanth_FTE.jpg",
+    Vasanth: "employee_list/0021 vasanth.jpg",
+    Jeevan: "employee_list/0022 Jeevan.jpg",
+    Thiru: "employee_list/0023 Thiru.jpg",
+    Arun: "employee_list/0024 Arun kumar.jpg",
+    Venkatesh: "employee_list/0025 venkatesh prabu.jpg",
+    Vinoth: "employee_list/0026 vinoth.jpg",
+    Chaitanya: "employee_list/0027 Chaitanya.JPG",
+    Pratik: "employee_list/0028 pratik.JPG",
+    Zahoor: "employee_list/0029 zahoor.JPG",
+    Arun_Buduri: "employee_list/0030 Arun Kumar Buduri.JPG",
+    Mayank: "employee_list/0031 Mayank.JPG",
+    Pranesh: "employee_list/0032 Pranesh.jpg",
+    Roshan: "employee_list/0033Roshan nair.jpg",
+    Manoj: "employee_list/0034-manoj.jpg",
+    Abishek: "employee_list/abishek.jpg",
+    Arul: "employee_list/arul.jpg",
+    Bharath: "employee_list/Barath.jpg",
+    Bhavna: "employee_list/Bhavna.jpg",
+    Sridhar: "employee_list/Sridhar.png"
+
+  }
