b5b6e4435ecd401ac4e9c958c13eb4e350d50c28
Bharath <bharathraja@live.com>
2013-03-07 13:12:45 +0530

    images added

--- bracket.js
+++ bracket.js
@@ -1,13 +1,57 @@
 bracket = {
   "name": "Overall Winner",
-  "winners": [
-    {
-      "name": "Finals",
-      "winners": [
-        {"name": "Winner Left 3"},
-        {"name": "Winner Left 4"}
-      ]
+  "winners": [{
+      "name": "Finalists",
+      "winners": [{
+          "name": "SemiFinalists",
+          "winners": [{
+              "name": "quarterFinalists",
+              "winners": [{
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "round-2",
+                      "winners": [{
+                          "name": "round-1",
+                        },
+                        {
+                          "name": "round-1"
+                        }]
+                    },
+                    {
+                      "name": "SemiFinal"
+                    }]
+                },
+                {
+                  "name": "round-4"
+                }]
+            },
+            {
+              "name": "quarterFinalists"
+            }]
+        },
+
+        {
+          "name": "SemiFinalists",
+          "winners": [{
+              "name": "SemiFinal"
+            },
+            {
+              "name": "SemiFinal"
+            }]
+        }]
     },
-    {"name": "Finals"}
-  ]
+
+    {
+      "name": "Finalists",
+      "winners": [{
+          "name": "SemiFinalists"
+        },
+        {
+          "name": "SemiFinalists"
+        }]
+    }]
+}
+
+images={
+  0: "employee_list/0004 SatyaKaliki.jpg"
 }
