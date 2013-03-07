bf934ee23e6dc93172c5ead8759eafdf0134b2af
Bharath <bharathraja@live.com>
2013-03-07 14:13:35 +0530

    image urls. size to 35. winner styles. few in list. local d3

--- bracket.js
+++ bracket.js
@@ -1,5 +1,5 @@
 bracket = {
-  "name": "Overall Winner",
+  "name": "Winner",
   "winners": [{
       "name": "Finalists",
       "winners": [{
@@ -11,32 +11,104 @@ bracket = {
                   "winners": [{
                       "name": "round-2",
                       "winners": [{
-                          "name": "round-1",
+                          "name": "Arun_Buduri",
                         },
                         {
-                          "name": "round-1"
+                          "name": "Arun"
                         }]
                     },
                     {
-                      "name": "SemiFinal"
+                      "name": "round-2",
+                      "winners": [{
+                          "name": "Vinoth",
+                        },
+                        {
+                          "name": "Chaitanya"
+                        }]
                     }]
                 },
                 {
-                  "name": "round-4"
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "round-2",
+                      "winners": [{
+                          "name": "Prem",
+                        },
+                        {
+                          "name": "Zahoor"
+                        }]
+                    },
+                    {
+                      "name": "Praveen"
+                    }]
                 }]
             },
             {
-              "name": "quarterFinalists"
+              "name": "quarterFinalists",
+              "winners": [{
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "Kishore"
+                    },
+                    {
+                      "name": "Pratik"
+                    }]
+                },
+                {
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "round-2"
+                    },
+                    {
+                      "name": "round-2"
+                    }]
+                }]
             }]
         },
 
         {
           "name": "SemiFinalists",
           "winners": [{
-              "name": "SemiFinal"
+              "name": "quarterFinalists",
+              "winners": [{
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "round-2"
+                    },
+                    {
+                      "name": "round-2"
+                    }]
+                },
+                {
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "round-2"
+                    },
+                    {
+                      "name": "round-2"
+                    }]
+                }]
             },
             {
-              "name": "SemiFinal"
+              "name": "quarterFinalists",
+              "winners": [{
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "round-2"
+                    },
+                    {
+                      "name": "round-2"
+                    }]
+                },
+                {
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "round-2"
+                    },
+                    {
+                      "name": "round-2"
+                    }]
+                }]
             }]
         }]
     },
@@ -44,14 +116,141 @@ bracket = {
     {
       "name": "Finalists",
       "winners": [{
-          "name": "SemiFinalists"
+          "name": "SemiFinalists",
+          "winners": [{
+              "name": "quarterFinalists",
+              "winners": [{
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "round-2"
+                    },
+                    {
+                      "name": "round-2"
+                    }]
+                },
+                {
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "round-2"
+                    },
+                    {
+                      "name": "round-2"
+                    }]
+                }]
+            },
+            {
+              "name": "quarterFinalists",
+              "winners": [{
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "round-2"
+                    },
+                    {
+                      "name": "round-2"
+                    }]
+                },
+                {
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "round-2"
+                    },
+                    {
+                      "name": "round-2"
+                    }]
+                }]
+            }]
         },
         {
-          "name": "SemiFinalists"
+          "name": "SemiFinalists",
+          "winners": [{
+              "name": "quarterFinalists",
+              "winners": [{
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "round-2"
+                    },
+                    {
+                      "name": "round-2"
+                    }]
+                },
+                {
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "round-2"
+                    },
+                    {
+                      "name": "round-2"
+                    }]
+                }]
+            },
+            {
+              "name": "quarterFinalists",
+              "winners": [{
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "round-2"
+                    },
+                    {
+                      "name": "round-2"
+                    }]
+                },
+                {
+                  "name": "round-3",
+                  "winners": [{
+                      "name": "round-2"
+                    },
+                    {
+                      "name": "round-2"
+                    }]
+                }]
+            }]
         }]
     }]
 }
 
-images={
-  0: "employee_list/0004 SatyaKaliki.jpg"
-}
+  imagesList = {
+    "Winner": "winner_profile.png",
+    "Finalists": "empty_profile.gif",
+    "SemiFinalists": "empty_profile.gif",
+    "quarterFinalists": "empty_profile.gif",
+    "round-1": "empty_profile.gif",
+    "round-2": "empty_profile.gif",
+    "round-3": "empty_profile.gif",
+    "round-4": "empty_profile.gif",
+    "Sanjay": "employee_list/0001 - SanjayParthasarathy.jpg",
+    Satya: "employee_list/employee_list/0004 SatyaKaliki.jpg",
+    Jonah: "employee_list/0003 Jonah.jpg",
+    Rajesh: "employee_list/0005 RajeshMupalla.jpg",
+    Srikanth: "employee_list/0006 Srikanth.jpg",
+    Praveen: "employee_list/0008 Praveen.jpg",
+    Anirudh: "employee_list/0009 Anirudh.jpg",
+    RajeshManikka: "employee_list/0010 RajeshManikka.jpg",
+    Sriram: "employee_list/0011 Sriram.jpg",
+    Prem: "employee_list/0012 prem.jpg",
+    Kishore: "employee_list/0013 kishore.jpg",
+    Karthika: "employee_list/0014 Karthika_Seles.jpg",
+    Archanaa: "employee_list/0015 Archanaa.jpg",
+    Aruna: "employee_list/0016 Aruna.jpg",
+    Sandipan: "employee_list/0017 Sandipan.jpg",
+    Ashwanth: "employee_list/0019 Ashwanth_FTE.jpg",
+    vasanth: "employee_list/0021 vasanth.jpg",
+    Jeevan: "employee_list/0022 Jeevan.jpg",
+    Thiru: "employee_list/0023 Thiru.jpg",
+    Arun: "employee_list/0024 Arun kumar.jpg",
+    Venkatesh: "employee_list/0025 venkatesh prabu.jpg",
+    Vinoth: "employee_list/0026 vinoth.jpg",
+    Chaitanya: "employee_list/0027 Chaitanya.JPG",
+    Pratik: "employee_list/0028 pratik.JPG",
+    Zahoor: "employee_list/0029 zahoor.JPG",
+    Arun_Buduri: "employee_list/0030 Arun Kumar Buduri.JPG",
+    Mayank: "employee_list/0031 Mayank.JPG",
+    Pranesh: "employee_list/0032 Pranesh.jpg",
+    Roshan: "employee_list/0033Roshan nair.jpg",
+    Manoj: "employee_list/0034-manoj.jpg",
+    Abishek: "employee_list/abishek.jpg",
+    Arul: "employee_list/arul.jpg",
+    Bharath: "employee_list/Barath.jpg",
+    Bhavna: "employee_list/Bhavna.jpg",
+    Sridhar: "employee_list/Sridhar.png"
+
+  }
