78dfcb0396141c6f4eb420c0b7c3eb9f1f88f9ae
Bharath <bharathraja@live.com>
2013-03-07 10:55:00 +0530

    initial commit: de basic bracket layout set

---
+++ bracket.js
@@ -0,0 +1,13 @@
+bracket = {
+  "name": "Overall Winner",
+  "winners": [
+    {
+      "name": "Finals",
+      "winners": [
+        {"name": "Winner Left 3"},
+        {"name": "Winner Left 4"}
+      ]
+    },
+    {"name": "Finals"}
+  ]
+}
